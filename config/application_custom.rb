77458090332a420612c4ff777dd0b31a095e8213
Consul <consul@localhost.localdomain>
2017-07-06 17:16:50 +0200

    Init of custom file for CDJ from Consul (all except locale)

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,7 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :fr
+    config.i18n.available_locales = [:fr]
+    config.i18n.fallbacks = {'es' => 'fr', 'en' => 'fr'}
   end
 end
